d1f2f7a8e5e30cca2ae6ac7db87f9835049ed8ca
Caique Castro <castro.caique@gmail.com>
2020-04-09 14:59:26 -0300

    Fix entities load glob for typeorm

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,8 +2,6 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'postgres',
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    entities: [__dirname + '/../**/*.entity.js'],
+    entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: true,
 };
