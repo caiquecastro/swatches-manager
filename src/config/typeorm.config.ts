f4d9baf9109a519fcdf75e670e1a36e302e582d8
Caique Castro <castro.caique@gmail.com>
2020-04-09 16:01:54 -0300

    Add missing credentials config for typeorm

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,6 +2,9 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'postgres',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: true,
 };
