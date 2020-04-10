8d6d406c3c331b73a716269e5666e0e6701e69d6
Caique Castro <castro.caique@gmail.com>
2020-04-10 10:00:43 -0300

    feature: Bootstrap frontend application
    Add css framework
    Add navbar component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <swatch-navbar />
+    <div class="container py-3">
+      <router-view />
     </div>
-    <router-view />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import SwatchNavbar from "@/components/Navbar";
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  components: {
+    SwatchNavbar
   }
-}
+};
+</script>
+
+<style lang="scss">
+@import "~bootstrap/scss/bootstrap";
 </style>
