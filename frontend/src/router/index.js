d01eff1678d0d4405c4c36fcf57f03a0f8f54a65
Caique Castro <castro.caique@gmail.com>
2020-04-10 13:40:47 -0300

    feature: Add views components
    Add pages to list, create and view swatches details
    Add steps on frontend workflow to upload to s3

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,28 +1,35 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import SwatchList from "../views/Swatches/List";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    redirect: "/swatches"
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: "/swatches",
+    component: SwatchList
+  },
+  {
+    path: "/swatches/new",
+    name: "NewSwatch",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "swatches" */ "../views/Swatches/Form.vue")
+  },
+  {
+    path: "/swatches/:id",
+    name: "SwatchDetails",
+    component: () =>
+      import(/* webpackChunkName: "swatches" */ "../views/Swatches/Details.vue")
   }
 ];
 
 const router = new VueRouter({
   mode: "history",
+  linkActiveClass: "active",
   base: process.env.BASE_URL,
   routes
 });
