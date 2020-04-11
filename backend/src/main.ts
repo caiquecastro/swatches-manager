69c38b17891e544e771c21b4b08fb29b260858a1
Caíque de Castro Soares da Silva <castro.caique@gmail.com>
2020-04-11 15:07:08 -0300

    feature: New File Field to upload swatch image (#20)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,3 +1,4 @@
+import { json } from 'body-parser';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -5,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors();
+  app.use(json({ limit: '1mb' }));
   await app.listen(3000);
 }
 bootstrap();
