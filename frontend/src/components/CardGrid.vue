dd5963e472ea2b13a84b2f1598052a65f0967474
Caique Castro <castro.caique@gmail.com>
2020-04-11 10:23:03 -0300

    fix: Adjust card grid for mobile

---
+++ frontend/src/components/CardGrid.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="row">
+    <div
+      :key="swatch.id"
+      v-for="swatch in swatches"
+      class="col-12 col-md-4 col-lg-3"
+    >
+      <swatch-card :swatch="swatch" @toggleActive="toggleActive(swatch)" />
+    </div>
+  </div>
+</template>
+
+<script>
+import SwatchCard from "@/components/Card";
+
+export default {
+  components: {
+    SwatchCard
+  },
+  props: {
+    swatches: {}
+  }
+};
+</script>
