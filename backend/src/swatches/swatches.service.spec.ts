35e06da7ebc777d63c1175a3f42fa032789bca66
Caique Castro <castro.caique@gmail.com>
2020-04-16 22:15:03 -0300

    fix test

--- backend/src/swatches/swatches.service.spec.ts
+++ backend/src/swatches/swatches.service.spec.ts
@@ -6,7 +6,7 @@ import { Swatch } from './swatch.entity';
 describe('SwatchesService', () => {
   let service: SwatchesService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [SwatchesService],
       imports: [
